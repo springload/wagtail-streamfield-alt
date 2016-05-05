20e232b394371532fc72139c8ce088f505cb2ac9
Vincent Audebert <vincent.audebert@yahoo.fr>
2016-05-05 16:20:11 +1200

    Fixing image block has a value or not

--- streamfield_alt/static/streamfield_alt/blocks/field_block.js
+++ streamfield_alt/static/streamfield_alt/blocks/field_block.js
@@ -72,23 +72,25 @@ export class ImageChooserBlock extends FieldBlock {
         return <div className={`field model_choice_field widget-admin_image_chooser fieldname-${this.props.schema.label.toLowerCase()} ${this.props.schema.classname}`}>
             <div className="field-content">
                 <div className="input">
-                    <div id={`${this.props.path}-chooser`} className="chooser image-chooser">
+                    <div id={`${this.props.path}-chooser`} className={`chooser image-chooser ${this.props.value === null ? 'blank' : ''}`}>
                         <div className="chosen">
-                            <div className="preview-image">
-                                <img alt="Wagtail collects insects by Margrit" className="show-transparency" src="/media/images/wagtail_collects_insects_by_Maggi_94.max-165x165.jpg" height="102" width="165" />
-                            </div>
+                                <div className="preview-image">
+                                    <img alt="Wagtail collects insects by Margrit" className="show-transparency" src={this.props.value} height="102" width="165" />
+                                </div>
                             <ul className="actions">
                                 <li>
-                                    <button type="button" className="action-choose button-small button-secondary">Choose another image</button>
+                                    <button type="button" className="button action-choose button-small button-secondary">Choose another image</button>
                                 </li>
-                                <li><a href={`/admin/images/${this.props.value}/`} className="edit-link button button-small button-secondary" target="_blank">Edit this image</a> </li>
+                                <li>
+                                    <a href={`/admin/images/${this.props.value}/`} className="button edit-link button-small button-secondary" target="_blank">Edit this image</a>
+                                </li>
                             </ul>
                         </div>
                         <div className="unchosen">
-                            <button type="button" className="action-choose button-small button-secondary">Choose an image</button>
+                            <button type="button" className="button action-choose button-small button-secondary">Choose an image</button>
                         </div>
                     </div>
-                    <input id={this.props.path} name={this.props.path} placeholder={this.props.schema.label} value={this.props.value} type="hidden" onChange={e => this.setValue(e.target.value)} />
+                    <input id={this.props.path} name={this.props.path} placeholder={this.props.schema.label} value={this.props.value === null ? '' : this.props.value} type="hidden" onChange={e => this.setValue(e.target.value)} />
                 </div>
             </div>
         </div>
