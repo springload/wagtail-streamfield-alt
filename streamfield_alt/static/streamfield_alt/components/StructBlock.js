7971b823511ed05916db202fa334a0dc5360b46d
Vincent Audebert <vincent.audebert@yahoo.fr>
2016-06-01 15:47:17 +1200

    Fixing structblock display bug... Now needs to keep values correctly

--- streamfield_alt/static/streamfield_alt/components/StructBlock.js
+++ streamfield_alt/static/streamfield_alt/components/StructBlock.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { BLOCK_TYPES_REGISTRY } from '../config';
 
-export class StructBlock extends React.Component {
+export default class StructBlock extends React.Component {
     renderBlock(value, schema, path) {
         const Component = BLOCK_TYPES_REGISTRY[schema.type];
         return <Component value={value} schema={schema} path={path} />;
     }
-    
+
     render() {
         const fields = [];
 
@@ -17,7 +18,7 @@ export class StructBlock extends React.Component {
             fields.push(
                 <li key={field}>
                     <label for={path}>{schema.label}</label>
-                    {renderBlock(this.props.store, value, schema, path)}
+                    {this.renderBlock(value, schema, path)}
                 </li>
             );
         }
